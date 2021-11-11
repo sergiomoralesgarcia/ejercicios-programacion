a5ba3679bb9ef88190b0c9c05a1d34123375fe76
Sergio Morales García <moralesgarcia300@dgmail.com>
2021-11-11 10:33:06 +0100

    Bucles

---
+++ Tema05/Ejercicio12.java
@@ -0,0 +1,20 @@
+/**
+ * Escribe un programa que muestre los n primeros términos de la serie de
+ * Fibonacci. El primer término de la serie de Fibonacci es 0, el segundo es 1 y
+ * el resto se calcula sumando los dos anteriores, por lo que tendríamos que los
+ * términos son 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144… El número n se debe
+ * introducir por teclado.
+ *
+ * @author Sergio Morales García
+ */
+public class Ejercicio12 {
+    public static void main(String[] args) {
+      System.out.println("Este programa va a mostrar los n primeros términos de la serie de Fibonacci. ");
+      System.out.println("Introduzca los n priemros números: ");
+      int numeros = Integer.parseInt(System.console().readLine());
+
+      for (int i = numeros; i < numeros + 5; i++) {
+      System.out.printf("%4d %6d %8d\n", i, i * i, i * i * i);
+      }
+    }
+  }
