584d4f8b72faebe4ee0f5c572dae22100936be29
Sergio Morales García <moralesgarcia300@gmail.com>
2021-11-18 10:12:00 +0100

    Bucles

--- Tema05/Ejercicio22.java
+++ Tema05/Ejercicio22.java
@@ -4,23 +4,23 @@
  * @author Sergio Morales García
  */
 public class Ejercicio22 {
-    public static void main(String[] args) {
-      System.out.println("Este programa calcula si un número introducido por teclado es primo o no. ");
-      System.out.println("Introduzca el número: ");
-      int numero = Integer.parseInt(System.console().readLine());
-  
-      boolean primo = true;
-  
-      for (int i = 2; i < numero; i++ ) {
-        if ((numero % i) == 0){
+  public static void main(String[] args) {
+    System.out.println("Este programa calcula si un número introducido por teclado es primo o no. ");
+    System.out.println("Introduzca el número: ");
+    int numero = Integer.parseInt(System.console().readLine());
+
+    boolean primo = true;
+
+    for (int i = 2; i < numero; i++) {
+      if ((numero % i) == 0) {
         primo = false;
-        } 
-      }  
-        
-      if (primo) {
-        System.out.println("El número es primo");
-      } else {
-        System.out.println("El número no es primo");
-      }  
+      }
+    }
+
+    if (primo) {
+      System.out.println("El número es primo");
+    } else {
+      System.out.println("El número no es primo");
     }
   }
+}
