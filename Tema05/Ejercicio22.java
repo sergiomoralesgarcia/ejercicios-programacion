fda663c7604bc9faa892e9a044157f2106d47aaa
Sergio Morales García <moralesgarcia300@gmail.com>
2021-11-18 10:14:52 +0100

    Bucles

--- Tema05/Ejercicio22.java
+++ Tema05/Ejercicio22.java
@@ -7,7 +7,7 @@ public class Ejercicio22 {
   public static void main(String[] args) {
     System.out.println("Este programa Muestra por pantalla todos los números primos entre 2 y 100. ");
 
-    for (int i = 2; i < numero; i++) {
+    for (int i = 2; i < 101; i++) {
       if ((numero % i) == 0) {
         primo = false;
       }
