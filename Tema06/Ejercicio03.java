a2db5960f1c12e73bfd490ff1a3f883f416fa7e8
Sergio Morales García <moralesgarcia300@gmail.com>
2021-11-21 18:37:06 +0100

    Numeros aleatorios

--- Tema06/Ejercicio03.java
+++ Tema06/Ejercicio03.java
@@ -1,23 +1,19 @@
 /**
- * Realiza un programa que muestre al azar el nombre de una carta de la baraja
- * francesa. Esta baraja está dividida en cuatro palos: picas, corazones,
- * diamantes y tréboles. Cada palo está formado por 13 cartas, de las cuales 9
- * cartas son numerales y 4 literales: 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K y A
- * (que sería el 1). Para convertir un número en una cadena de caracteres
- * podemos usar String.valueOf(n).
+ * Igual que el ejercicio anterior pero con la baraja española. Se utilizará la baraja
+ * de 40 cartas: 2, 3, 4, 5, 6, 7, sota, caballo, rey y as.
  *
  * @author Sergio Morales García
  */
-public class Ejercicio02 {
+public class Ejercicio03 {
   public static void main(String[] args) {
     System.out.println("Muestra una carta de la baraja francesa al azar:");
-    
+
     String nombreCarta = "";
     String numeroCarta = "";
 
     for (int i = 1; i <= 1; i++) {
       int nombre = (int) (Math.random() * 4) + 1; 
-
+      
       switch (nombre) {
       case 1:
         nombreCarta = "picas";
@@ -26,7 +22,8 @@ public class Ejercicio02 {
         nombreCarta = "diamantes";
         break;
       case 3:
-        nombreCarta = "treboles";;
+        nombreCarta = "treboles";
+        ;
         break;
       case 4:
         nombreCarta = "corazones";
@@ -36,10 +33,10 @@ public class Ejercicio02 {
 
       for (int n = 1; n <= 1; n++) {
         int numero = (int) (Math.random() * 13) + 1;
-        
+
         switch (numero) {
         case 1:
-          numeroCarta ="As";
+          numeroCarta = "As";
           break;
         case 11:
           numeroCarta = "J";
@@ -55,7 +52,7 @@ public class Ejercicio02 {
         }
         System.out.println("La carta es un " + numeroCarta + " de " + nombreCarta);
       }
-    }  
+    }
     System.out.println();
-  }  
+  }
 }
