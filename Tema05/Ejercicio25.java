8e275320f1c9cdcaf4f9bfa0722c448aa1c870ca
Sergio Morales García <moralesgarcia300@gmail.com>
2021-12-11 18:35:37 +0100

    Bucles

--- Tema05/Ejercicio25.java
+++ Tema05/Ejercicio25.java
@@ -1,3 +1,21 @@
+/**
+ * Realiza un programa que pida un número por teclado y que luego muestre ese
+ * número al revés.
+ * 
+ * @author Sergio Morales García
+ */
 public class Ejercicio25 {
-    
+  public static void main(String[] args) {
+    System.out.print("Introduzca un número entero: ");
+
+    int numeroIntroducido = Integer.parseInt(System.console().readLine());
+    int numero = numeroIntroducido;
+    int volteado = 0;
+
+    while (numero > 0) {
+      volteado = (volteado * 10) + (numero % 10);
+      numero /= 10;
+      } 
+    System.out.println("Si le damos la vuelta al " + numeroIntroducido + " tenemos el " + volteado);
+  }
 }
