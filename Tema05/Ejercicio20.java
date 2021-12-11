78ef87da711c50fc3950b810401b5463c1121ce2
Sergio Morales García <moralesgarcia300@gmail.com>
2021-12-11 23:06:44 +0100

    Bucles

--- Tema05/Ejercicio20.java
+++ Tema05/Ejercicio20.java
@@ -1,18 +1,48 @@
 /**
- * Realiza un programa que pinte una pirámide por pantalla. La altura se debe
- * pedir por teclado. El carácter con el que se pinta la pirámide también se
- * debe pedir por teclado.
+ * Igual que el ejercicio anterior pero esta vez se debe pintar una pirámide hueca.
  * 
  * @author Sergio Morales García
  */
 public class Ejercicio20 {
   public static void main(String[] args) {
-    /* System.out.println("Este programa dibuja una piramide en pantalla.");
-    System.out.print("Introduzca la altura de la pirámide: ");
-    int altura = Integer.parseInt(System.console().readLine());
-    System.out.print("Introduzca el carácter con el que se dibuja la pirámide: ");
-    String caracter = System.console().readLine(); */
+    System.out.print("Por favor, introduzca la altura de la pirámide: ");
+    int alturaIntroducida = Integer.parseInt(System.console().readLine());
 
+    System.out.print("Introduzca el carácter de relleno: ");
+    String relleno = System.console().readLine();
+    
+    int altura = 1;
+    int i = 0;
+    int espaciosPorDelante = alturaIntroducida - 1;
+    int espaciosInternos = 0;
+    
+    while (altura < alturaIntroducida) {
+      
+      // inserta espacios delante
+      for (i = 1; i <= espaciosPorDelante; i++) {
+        System.out.print(" ");
+      }
+      
+      // pinta la línea
+      System.out.print(relleno);
+      for (i = 1; i < espaciosInternos; i++) {
+        System.out.print(" ");
+      }
+      
+      if (altura>1) {
+        System.out.print(relleno);
+      }
+      
+      System.out.println();
+      altura++;
+      espaciosPorDelante--;
+      espaciosInternos += 2;
+    } // while ////////////////////////////
+    
+    // base de la pirámide
+    for (i = 1; i < altura*2; i++) {
+      System.out.print(relleno);
+    }
   }
 }
 
