d875d9c1a5b2759eb9ad8857de03c9f322532a76
Sergio Morales García <moralesgarcia300@gmail.com>
2021-12-11 20:08:33 +0100

    Bucles

--- Tema05/Ejercicio15.java
+++ Tema05/Ejercicio15.java
@@ -3,7 +3,7 @@
  * positivo (exponente), saque por pantalla todas las potencias con base el
  * numero dado y exponentes entre uno y el exponente introducido. No se deben
  * utilizar funciones de exponenciación. Por ejemplo, si introducimos el 2 y el
- * 5, se deberán mostrar 21, 22, 23 , 24 y 25 .
+ * 5, se deberán mostrar 2¹, 2², 2³, 2⁴ y 2⁵.
  * 
  * @author Sergio Morales García
  */
@@ -15,9 +15,19 @@ public class Ejercicio15 {
     System.out.println("Introduzca el exponente: ");
     int exponente = Integer.parseInt(System.console().readLine());
 
+    int potencia;
+    int exp;
+
     for (int i = 1; i <= exponente; i++) {
-      System.out.println("El resultado es " + base + "^" + i);
-      System.out.println("----------------------");
+      potencia = 1;
+      exp = i;
+
+      for (int j = 0; j < exp; j++) {
+        potencia *= base;
+      }
+
+      System.out.println("El resultado es " + base + "^" + i + " = " + potencia);
+      System.out.println("-----------------------------");
     }
   }
 }
