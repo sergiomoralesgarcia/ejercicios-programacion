26e5e671ce439293b46578dcd16680c8ae29c47a
Sergio Morales García <moralesgarcia300@gmail.com>
2021-12-11 13:51:49 +0100

    Bucles

---
+++ Tema05/Ejercicio24.java
@@ -0,0 +1,30 @@
+/**
+ * Escribe un programa que permita ir introduciendo una serie indeterminada
+ * de números mientras su suma no supere el valor 10000. Cuando esto último
+ * ocurra, se debe mostrar el total acumulado, el contador de los números
+ * introducidos y la media.
+ * 
+ * @author Sergio Morales García
+ */
+public class Ejercicio24 {
+  public static void main(String[] args) {
+    System.out.println("Vaya introduciendo números.");
+    System.out.println("Se parará cuando la suma total no supere los 1000.");
+
+    int numeroIntroducido;
+    int suma = 0;
+    int numeroDeElementos = 0;
+
+    do {
+      numeroIntroducido = Integer.parseInt(System.console().readLine());
+
+      suma += numeroIntroducido;
+      numeroDeElementos++;
+
+    } while (suma <= 1000);
+
+    System.out.println("Has llegado a la cantidad de " + suma);
+    System.out.println("Ha introducido " + numeroDeElementos + " números");
+    System.out.println("La media total " + suma / numeroDeElementos);
+  }
+}
