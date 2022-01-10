dcfcd5c6a14aa673c703de5cec3ea378f5495104
Sergio Morales García <moralesgarcia300@gmail.com>
2022-01-10 09:48:59 +0100

    Arrays

--- Tema07_Bidimensional/Ejer01_07.java
+++ Tema07_Bidimensional/Ejer01_07.java
@@ -1,12 +1,11 @@
 /**
- * @author Sergio Morales García
- *
  * Define un array de números enteros de 3 filas por 6 columnas con nombre num
  * y asigna los valores según la siguiente tabla. Muestra el contenido de todos
  * los elementos del array dispuestos en forma de tabla como se muestra en la
  * figura.
+ * 
+ * @author Sergio Morales García
  */
-
 public class Ejer01_07 {
   public static void main(String[] args)
       throws InterruptedException { // Se añade esta línea para poder usar sleep
