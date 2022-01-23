f85207a18dd7a3e6c1fd692d26b02d0944411793
Sergio Morales García <moralesgarcia300@gmail.com>
2022-01-23 11:56:32 +0100

    Funciones

---
+++ Tema08/Ejer01_08.java
@@ -0,0 +1,30 @@
+/**
+ * 8. Funciones
+ *
+ * Ejercicio 1
+ *
+ * @author Sergio Morales García
+ */
+public class Ejer01_08{ 
+    public static void main(String[] arg){
+
+        System.out.println(esCapicua(56788765));
+    
+    }
+    public static boolean esCapicua(long numero){
+
+        long volteado = 0;
+
+        while (numero > 0) {
+            volteado = (volteado * 10) + (numero % 10);
+            numero /= 10;
+          } // while
+        
+        if (volteado == 56788765) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+}
